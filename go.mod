9d023c9727d125dddc01eb23e0075c35eeddc5c2
Christian <Christian.Huff.DEV@gmail.com>
2019-12-03 17:40:20 +0000

    Start implemention of interactive shell
    Implement a barebone example demonstrating how to start the interactive
    shell, add commands, and exeute the commands.
    
    github-mirror/reapy#4

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/ChristianHuff-DEV/reapy
 go 1.13
 
 require (
+	github.com/c-bata/go-prompt v0.2.3
+	github.com/mattn/go-runewidth v0.0.7 // indirect
+	github.com/mattn/go-tty v0.0.3 // indirect
 	gopkg.in/yaml.v2 v2.2.7
 	gopkg.in/yaml.v3 v3.0.0-20191120175047-4206685974f2
 )
--- reapy.go
+++ reapy.go
@@ -2,16 +2,43 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	c "github.com/ChristianHuff-DEV/reapy/config"
 	"github.com/ChristianHuff-DEV/reapy/model"
+	"github.com/c-bata/go-prompt"
 )
 
 var config model.Config
 
 func main() {
-	for _, plan := range config.Plans {
-		c.Execute(plan)
+	// for _, plan := range config.Plans {
+	// 	c.Execute(plan)
+	// }
+	p := prompt.New(executor, completer)
+	p.Run()
+}
+
+// completer provides the available commands
+func completer(document prompt.Document) []prompt.Suggest {
+	return []prompt.Suggest{
+		{Text: "help"},
+		{Text: "execute"},
+		{Text: "exit"},
 	}
+}
+
+func executor(command string) {
+	switch command {
+	case "help":
+		log.Print("Executed help")
+	case "execute":
+		log.Print("Execute execute")
+	case "exit":
+		os.Exit(0)
+	}
+
 }
 
 // init will read the config yaml before starting the app itself
