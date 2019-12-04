12c9f603aea119d801720243e8ae245100d4fec8
Christian <Christian.Huff.DEV@gmail.com>
2019-12-04 18:06:33 +0000

    Example to show the use of the interactive shell
    Implement basic example that shows how to add commands and execute them.
    
    github-mirror/reapy#4

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/ChristianHuff-DEV/reapy
 go 1.13
 
 require (
+	github.com/AlecAivazis/survey/v2 v2.0.5
+	github.com/briandowns/spinner v1.8.0
 	github.com/c-bata/go-prompt v0.2.3
 	github.com/mattn/go-runewidth v0.0.7 // indirect
 	github.com/mattn/go-tty v0.0.3 // indirect
-	gopkg.in/yaml.v2 v2.2.7
+	github.com/pkg/term v0.0.0-20190109203006-aa71e9d9e942 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20191120175047-4206685974f2
 )
--- reapy.go
+++ reapy.go
@@ -2,11 +2,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/AlecAivazis/survey/v2"
 	c "github.com/ChristianHuff-DEV/reapy/config"
 	"github.com/ChristianHuff-DEV/reapy/model"
+	"github.com/briandowns/spinner"
 	"github.com/c-bata/go-prompt"
 )
 
@@ -32,13 +36,52 @@ func completer(document prompt.Document) []prompt.Suggest {
 func executor(command string) {
 	switch command {
 	case "help":
+		showWait()
 		log.Print("Executed help")
+		showChecklist()
 	case "execute":
 		log.Print("Execute execute")
+		askQuestion()
 	case "exit":
 		os.Exit(0)
 	}
 
+}
+
+func askQuestion() {
+	var qs = []*survey.Question{
+		{
+			Name:      "name",
+			Prompt:    &survey.Input{Message: "What is your name?"},
+			Validate:  survey.Required,
+			Transform: survey.Title,
+		}}
+	answers := struct {
+		Name string // survey will match the question and field names
+	}{}
+	err := survey.Ask(qs, &answers)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("chose %s.\n", answers.Name)
+}
+
+func showWait() {
+	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond) // Build our new spinner
+	s.Start()                                                    // Start the spinner
+	time.Sleep(4 * time.Second)                                  // Run for some time to simulate work
+	s.Stop()
+}
+
+func showChecklist() {
+	days := []string{}
+	prompt := &survey.MultiSelect{
+		Message: "What days do you prefer:",
+		Options: []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"},
+	}
+	survey.AskOne(prompt, &days)
 }
 
 // init will read the config yaml before starting the app itself
