8ed9106960440ccaea375b29e65811776a71cf00
Christian <Christian.Huff.DEV@gmail.com>
2019-12-11 17:36:27 +0000

    Add comments
    github-mirror/reapy#6

--- cli/validator.go
+++ cli/validator.go
@@ -5,26 +5,24 @@ import (
 	"regexp"
 )
 
+// validateVariables ensures that all defined variables are in the correct format
 func validateVariables(variablesYaml map[string]interface{}) error {
-
 	for key := range variablesYaml {
 		err := validateVariableName(key)
 		if err != nil {
 			return err
 		}
 	}
-
 	return nil
 }
 
+// validateVariableName ensures that the given name contains only letter, number, "-" and "_".
 func validateVariableName(name string) error {
-
 	regex, _ := regexp.Compile("^[_A-z0-9]*((-|_)*[_A-z0-9])*$")
 	isValid := regex.MatchString(name)
 
 	if !isValid {
-		return fmt.Errorf("the variable name \"%s\" is invalid. Variable names can only consist out of characters, numbers, \"-\" and \"_\"", name)
+		return fmt.Errorf("the variable name \"%s\" is invalid. Variable names can only consist out of letters, numbers, \"-\" and \"_\"", name)
 	}
-
 	return nil
 }
