7cced5f4538f72422165ea7cd1b83bfed36887d1
Christian <Christian.Huff.DEV@gmail.com>
2019-12-21 17:44:22 +0000

    Add "reload" command to hot reload config
    Add the "reload" command. This command can be used to re-initialize the
    available plans based on the config yaml file.
    
    github-mirror/reapy#7

--- cli/commands.go
+++ cli/commands.go
@@ -2,17 +2,21 @@ package cli
 
 import (
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"os"
 	"time"
 
-	"github.com/AlecAivazis/survey/v2"
+	"github.com/ChristianHuff-DEV/reapy/model"
 	"github.com/briandowns/spinner"
 	"github.com/c-bata/go-prompt"
+	"github.com/gookit/color"
 )
 
-var baseSuggests = []prompt.Suggest{{Text: "help", Description: "Show available commands"}, {Text: "exit", Description: "Exit the application"}, {Text: "execute", Description: "Choose a plan to execute"}}
+var p *prompt.Prompt
 
-var baseFunctions = map[string]func(){"help": funcHelp, "exit": funcExit}
+var baseSuggests = []prompt.Suggest{{Text: "help", Description: "Show available commands"}, {Text: "exit", Description: "Exit the application"}, {Text: "execute", Description: "Choose a plan to execute"}, {Text: "reload", Description: "Reload plans from config"}}
+
+var baseFunctions = map[string]func(){"help": funcHelp, "exit": funcExit, "reload": funcReload}
 
 var funcHelp = func() {
 	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
@@ -25,38 +29,36 @@ var funcExit = func() {
 	os.Exit(0)
 }
 
-func askQuestion() {
-	var qs = []*survey.Question{
-		{
-			Name:      "name",
-			Prompt:    &survey.Input{Message: "What is your name?"},
-			Validate:  survey.Required,
-			Transform: survey.Title,
-		}}
-	answers := struct {
-		Name string // survey will match the question and field names
-	}{}
-	err := survey.Ask(qs, &answers)
+var funcReload = func() {
+	fmt.Println("funcReload")
+	InitializePlans()
+}
+
+// Start creates the prompt instance and runs it
+func Start() {
+	p = prompt.New(Executor, Completer)
+	p.Run()
+}
+
+// InitializePlans creates the global config instance that creates the plans definitions
+//
+// Can also be used to update the available plans.
+func InitializePlans() (err error) {
+	Config, err = readPlanDefinition()
 	if err != nil {
-		fmt.Println(err.Error())
-		return
+		color.Red.Printf("Error reading plans definition file: %s\n", err)
+		log.Fatal(err)
+		return err
 	}
-
-	fmt.Printf("chose %s.\n", answers.Name)
+	return nil
 }
 
-func showWait() {
-	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond) // Build our new spinner
-	s.Start()                                                    // Start the spinner
-	time.Sleep(4 * time.Second)                                  // Run for some time to simulate work
-	s.Stop()
-}
-
-func showChecklist() {
-	days := []string{}
-	prompt := &survey.MultiSelect{
-		Message: "What days do you prefer:",
-		Options: []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"},
+// readPlanDefinition parses a given config yaml file into the config instance
+func readPlanDefinition() (config model.Config, err error) {
+	log.Println("reading plans configuration file")
+	config, err = Extract("test.yaml")
+	if err != nil {
+		return config, err
 	}
-	survey.AskOne(prompt, &days)
+	return config, nil
 }
--- reapy.go
+++ reapy.go
@@ -2,14 +2,12 @@
 package main
 
 import (
-	log "github.com/sirupsen/logrus"
 	"os"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/ChristianHuff-DEV/reapy/cli"
-	"github.com/ChristianHuff-DEV/reapy/model"
-	"github.com/c-bata/go-prompt"
-	"github.com/gookit/color"
-	"github.com/x-cray/logrus-prefixed-formatter"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 )
 
 func main() {
@@ -19,23 +17,21 @@ func main() {
 	}
 	defer file.Close()
 
-	cli.Config, err = readPlanDefinition()
+	err = cli.InitializePlans()
 	if err != nil {
-		color.Red.Printf("Error reading plans definition file: %s\n", err)
 		log.Fatal(err)
 	}
 
-	p := prompt.New(cli.Executor, cli.Completer)
-	p.Run()
+	cli.Start()
 }
 
 func setupLogging() (*os.File, error) {
 	log.SetFormatter(&prefixed.TextFormatter{
-            DisableColors: true,
-            TimestampFormat : "2006-01-02 15:04:05",
-            FullTimestamp:true,
-						ForceFormatting: true,
-        },)
+		DisableColors:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+		FullTimestamp:   true,
+		ForceFormatting: true,
+	})
 
 	// Create log file
 	file, err := os.OpenFile("reapy.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
@@ -46,14 +42,4 @@ func setupLogging() (*os.File, error) {
 	log.SetOutput(file)
 	// Set file as log output
 	return file, nil
-}
-
-// readPlanDefinition parses a given config yaml file into the config instance
-func readPlanDefinition() (config model.Config, err error) {
-	log.Println("reading plans configuration file")
-	config, err = cli.Extract("test.yaml")
-	if err != nil {
-		return config, err
-	}
-	return config, nil
 }
