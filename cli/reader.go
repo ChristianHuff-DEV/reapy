f84b191a614413077aba6aef6a3ce492cd0fd9d6
Christian <Christian.Huff.DEV@gmail.com>
2019-12-21 15:20:00 +0000

    Add step to copy a folder
    rdd a step type that can be used to copy a folder.
    
    github-mirror/reapy#20

--- cli/reader.go
+++ cli/reader.go
@@ -203,6 +203,13 @@ func parseSteps(stepsYaml []interface{}, variables map[string]string) (steps []m
 					return steps, err
 				}
 				steps = append(steps, &step)
+			case step.KindCopyFolder:
+				step := stepDefinition.CopyFolder{}
+				err = step.FromConfig(stepYaml)
+				if err != nil {
+					return steps, err
+				}
+				steps = append(steps, &step)
 			default:
 				return steps, fmt.Errorf("Unknown step type \"%s\" used", kind)
 			}
